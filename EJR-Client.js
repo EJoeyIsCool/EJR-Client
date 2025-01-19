13e425ff58808965ae07b8bfce2e80448e6822e2
EJoeyIsCool <159656617+EJoeyIsCool@users.noreply.github.com>
2025-01-18 23:21:55 -0500

    Create EJR-Client.js

---
+++ EJR-Client.js
@@ -0,0 +1,8 @@
+/*
+Copy the code below fully.
+Paste it into your bookmarks bar or add a new bookmark and put the code inside the URL section.
+If it doesn't work, the site may not allow code from other sites to be pulled and run.
+If this happens, simply go to the MinifiedJavascriptCode.js file of this repository and do these steps with that much larger code.
+*/
+
+javascript:!function(){var e=document.createElement("script");e.src="https://EJoeyIsCool.github.io/EJR-Client/MinifiedJavascriptCode.js",e.onload=function(){e.remove()},e.onerror=function(){alert("Failed to load the script. Check if the website you are on has Javascript execution enabled."),e.remove()},document.body.appendChild(e)}();
