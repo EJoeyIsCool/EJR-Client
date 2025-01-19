c82389411d02dd78d182c8e1dfd616a36cb75a3b
EJoeyIsCool <159656617+EJoeyIsCool@users.noreply.github.com>
2025-01-19 17:13:18 -0500

    Update and rename EJR-Client.js to VeryShortCode.js

---
+++ VeryShortCode.js
@@ -0,0 +1,9 @@
+/*
+Copy the code below fully if you would prefer to have a much smaller snippet in your bookmarks.
+Paste it into your bookmarks bar or add a new bookmark and put the code inside the URL section.
+If it doesn't work, the site may not allow code from other sites to be pulled and run.
+In this case, go to https://ejoeyiscool.github.io/EJR-Client/index.html and drag the button into your bookmarks.
+Or, go to the MinifiedJavascriptCode.js file of this repository and do these steps with that much larger code.
+*/
+
+javascript:!function(){var e=document.createElement('script');e.src='https://EJoeyIsCool.github.io/EJR-Client/MinifiedCode.js',e.onload=function(){e.remove()},e.onerror=function(){alert('Failed to load the script. Check if the website you are on has Javascript execution enabled.'),e.remove()},document.body.appendChild(e)}();
