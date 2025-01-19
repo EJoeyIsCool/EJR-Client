13035c370a517a37304973458db8b70c456e84e5
EJoeyIsCool <159656617+EJoeyIsCool@users.noreply.github.com>
2025-01-18 23:39:31 -0500

    Update EJR-Client.js

--- EJR-Client.js
+++ EJR-Client.js
@@ -5,4 +5,4 @@ If it doesn't work, the site may not allow code from other sites to be pulled an
 If this happens, simply go to the MinifiedJavascriptCode.js file of this repository and do these steps with that much larger code.
 */
 
-javascript:!function(){var e=document.createElement("script");e.src="https://EJoeyIsCool.github.io/EJR-Client/MinifiedJavascriptCode.js",e.onload=function(){e.remove()},e.onerror=function(){alert("Failed to load the script. Check if the website you are on has Javascript execution enabled."),e.remove()},document.body.appendChild(e)}();
+javascript:!function(){var e=document.createElement('script');e.src='https://EJoeyIsCool.github.io/EJR-Client/MinifiedJavascriptCode.js',e.onload=function(){e.remove()},e.onerror=function(){alert('Failed to load the script. Check if the website you are on has Javascript execution enabled.'),e.remove()},document.body.appendChild(e)}();
